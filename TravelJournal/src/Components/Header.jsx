3bd353af96514e839512240044b35d8bb8188b16
arpit mishra <arpit.mishra.out@gmail.com>
2024-12-03 22:44:50 +0530

    Travel Journal Project NavBar

---
+++ TravelJournal/src/Components/Header.jsx
@@ -0,0 +1,19 @@
+
+
+export default function  Header()
+{
+    return (
+        <>
+        <nav className="navbar navbar-default">
+            <div className="container">
+                <div className="logo">
+                    <img src="https://i.ibb.co/MCfT6Dg/globe.png" alt="globe" width="50" height="50"/>
+                </div>
+                <div className="heading">
+                    <h3>my travel journal</h3>
+                </div>
+            </div>
+        </nav>
+        </>
+    );
+}
